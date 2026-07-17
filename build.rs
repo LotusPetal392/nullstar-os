4cf65473f6ed4214a63dc2dd2fce2ad32786f5b8
lotuspetalagent <lotuspetalagent@gmail.com>
2026-07-17 13:21:04 -0400

    Place userspace init on boot filesystem

--- build.rs
+++ build.rs
@@ -7,11 +7,16 @@ fn main() {
     let kernel_binary = PathBuf::from(
         env::var_os("CARGO_BIN_FILE_KERNEL_kernel").expect("kernel artifact path was not set"),
     );
+    let userspace_init = PathBuf::from(
+        env::var_os("CARGO_BIN_FILE_USERSPACE_init")
+            .expect("userspace init artifact path was not set"),
+    );
 
     let bios_image = output_directory.join("galactic-os-bios.img");
-
-    bootloader::BiosBoot::new(&kernel_binary)
-        .create_disk_image(&bios_image)
+    let mut image = bootloader::DiskImageBuilder::new(kernel_binary);
+    image.set_file(String::from("init"), userspace_init);
+    image
+        .create_bios_image(&bios_image)
         .expect("failed to create BIOS disk image");
 
     println!("cargo:rustc-env=BIOS_IMAGE={}", bios_image.display());
