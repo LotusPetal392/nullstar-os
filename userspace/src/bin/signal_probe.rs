8b8bce26aec071b030cccc6e293e3902ac5a68a6
lotuspetalagent <lotuspetalagent@gmail.com>
2026-07-18 21:12:38 -0400

    Migrate signal probe to the Rust runtime

--- userspace/src/bin/signal_probe.rs
+++ userspace/src/bin/signal_probe.rs
@@ -1,46 +1,18 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::global_asm, panic::PanicInfo};
+use userspace::syscall::{self, STDOUT};
 
-global_asm!(
-    r#"
-    .equ MESSAGE_BYTES, 21
+userspace::entry!(rust_main);
+userspace::panic_handler!();
 
-    .section .text._start,"ax"
-    .p2align 4
-    .global _start
-    .type _start,@function
-_start:
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [rip + message]
-    mov rdx, MESSAGE_BYTES
-    int 0x80
-    test rax, rax
-    js .Lfailure
-
-.Lyield_forever:
-    mov rax, 2
-    int 0x80
-    jmp .Lyield_forever
-
-.Lfailure:
-    mov rax, 3
-    mov rdi, 1
-    int 0x80
-    ud2
-.size _start, .-_start
-
-    .section .rodata,"a"
-message:
-    .ascii "signal probe running\n"
-"#,
-);
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+extern "C" fn rust_main(_initial_stack: *const usize) -> ! {
+    if syscall::write_all(STDOUT, b"signal probe running\n").is_err() {
+        syscall::exit(1);
+    }
     loop {
-        core::hint::spin_loop();
+        if syscall::yield_now().is_err() {
+            syscall::exit(1);
+        }
     }
 }
