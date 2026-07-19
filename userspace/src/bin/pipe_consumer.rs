da04d7deee19319dad4a57806f88e2c45f1ae2b8
lotuspetalagent <lotuspetalagent@gmail.com>
2026-07-18 21:12:10 -0400

    Migrate pipe consumer to the Rust runtime

--- userspace/src/bin/pipe_consumer.rs
+++ userspace/src/bin/pipe_consumer.rs
@@ -1,61 +1,23 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::global_asm, panic::PanicInfo};
+use userspace::syscall::{self, STDIN, STDOUT};
 
-global_asm!(
-    r#"
-    .section .text._start,"ax"
-    .p2align 4
-    .global _start
-    .type _start,@function
-_start:
-    sub rsp, 256
-.Lread:
-    mov rax, 5
-    xor rdi, rdi
-    mov rsi, rsp
-    mov rdx, 256
-    int 0x80
-    test rax, rax
-    js .Lfailure
-    jz .Ldone
+userspace::entry!(rust_main);
+userspace::panic_handler!();
 
-    mov r12, rsp
-    mov r13, rax
-.Lwrite:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, r12
-    mov rdx, r13
-    int 0x80
-    test rax, rax
-    jle .Lfailure
-    add r12, rax
-    sub r13, rax
-    jnz .Lwrite
-    jmp .Lread
-
-.Ldone:
-    add rsp, 256
-    mov rax, 3
-    xor rdi, rdi
-    int 0x80
-    ud2
-
-.Lfailure:
-    add rsp, 256
-    mov rax, 3
-    mov rdi, 1
-    int 0x80
-    ud2
-.size _start, .-_start
-"#,
-);
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+extern "C" fn rust_main(_initial_stack: *const usize) -> ! {
+    let mut buffer = [0_u8; 256];
     loop {
-        core::hint::spin_loop();
+        let count = match syscall::read(STDIN, &mut buffer) {
+            Ok(count) => count,
+            Err(_) => syscall::exit(1),
+        };
+        if count == 0 {
+            syscall::exit(0);
+        }
+        if syscall::write_all(STDOUT, &buffer[..count]).is_err() {
+            syscall::exit(1);
+        }
     }
 }
