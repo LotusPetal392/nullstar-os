830c20cec7abcfc0c56033a84dd6a1979e5f772e
lotuspetalagent <lotuspetalagent@gmail.com>
2026-07-18 21:11:39 -0400

    Migrate readline to the Rust runtime

--- userspace/src/bin/readline.rs
+++ userspace/src/bin/readline.rs
@@ -1,68 +1,26 @@
 #![no_std]
 #![no_main]
 
-use core::{arch::global_asm, panic::PanicInfo};
+use userspace::syscall::{self, STDERR, STDIN, STDOUT};
 
-global_asm!(
-    r#"
-    .section .text._start,"ax"
-    .p2align 4
-    .global _start
-    .type _start,@function
-_start:
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [rip + prompt]
-    mov rdx, 10
-    int 0x80
+userspace::entry!(rust_main);
+userspace::panic_handler!();
 
-    sub rsp, 256
-    mov rax, 5
-    xor rdi, rdi
-    mov rsi, rsp
-    mov rdx, 256
-    int 0x80
-    test rax, rax
-    js .Lfailure
-    mov r12, rax
-
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [rip + response_prefix]
-    mov rdx, 10
-    int 0x80
-
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, rsp
-    mov rdx, r12
-    int 0x80
-    add rsp, 256
-
-    mov rax, 3
-    xor rdi, rdi
-    int 0x80
-    ud2
-
-.Lfailure:
-    add rsp, 256
-    mov rax, 3
-    mov rdi, 1
-    int 0x80
-    ud2
-.size _start, .-_start
-
-    .section .rodata,"a"
-prompt:
-    .ascii "readline> "
-response_prefix:
-    .ascii "terminal: "
-"#,
-);
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {
-        core::hint::spin_loop();
+extern "C" fn rust_main(_initial_stack: *const usize) -> ! {
+    if syscall::write_all(STDOUT, b"readline> ").is_err() {
+        syscall::exit(1);
     }
+
+    let mut buffer = [0_u8; 256];
+    let count = match syscall::read(STDIN, &mut buffer) {
+        Ok(count) => count,
+        Err(_) => syscall::exit(1),
+    };
+    if syscall::write_all(STDOUT, b"terminal: ").is_err()
+        || syscall::write_all(STDOUT, &buffer[..count]).is_err()
+    {
+        let _ = syscall::write_all(STDERR, b"readline: write failed\n");
+        syscall::exit(1);
+    }
+    syscall::exit(0)
 }
