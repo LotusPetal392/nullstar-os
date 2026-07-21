1929ce804d75c8051338cbc0675d848380f1d804
lotuspetalagent <lotuspetalagent@gmail.com>
2026-07-21 08:47:51 -0400

    Route syscalls through platform dispatcher

--- kernel/src/process/mod.rs
+++ kernel/src/process/mod.rs
@@ -1,4 +1,14 @@
 pub(crate) mod elf;
 pub(crate) mod pipe;
 mod terminal;
-pub(crate) mod userspace;
+
+mod userspace_legacy {
+    include!("userspace.rs");
+    include!("userspace_platform.rs");
+}
+
+pub(crate) mod userspace {
+    pub use super::userspace_legacy::*;
+    pub use super::userspace_legacy::platform_syscall_interrupt_entry_address
+        as syscall_interrupt_entry_address;
+}
