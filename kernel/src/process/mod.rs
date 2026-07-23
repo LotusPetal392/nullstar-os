ab620cf472e112b0955cf95ec7449910b29b7c17
lotuspetalagent <lotuspetalagent@gmail.com>
2026-07-23 13:14:08 -0400

    Route syscalls through capability bootstrap entry

--- kernel/src/process/mod.rs
+++ kernel/src/process/mod.rs
@@ -10,6 +10,7 @@ mod userspace_legacy {
     include!("userspace_platform/process.rs");
     include!("userspace_platform/process_group_entry.rs");
     include!("userspace_platform/capability_entry.rs");
+    include!("userspace_platform/capability_grant_entry.rs");
 }
 
 pub(crate) mod userspace {
@@ -22,6 +23,8 @@ pub(crate) mod userspace {
             super::userspace_legacy::platform_syscall_interrupt_entry_address;
         let _process_group_entry: fn() -> x86_64::VirtAddr =
             super::userspace_legacy::process_group_syscall_interrupt_entry_address;
-        super::userspace_legacy::capability_syscall_interrupt_entry_address()
+        let _capability_entry: fn() -> x86_64::VirtAddr =
+            super::userspace_legacy::capability_syscall_interrupt_entry_address;
+        super::userspace_legacy::capability_grant_syscall_interrupt_entry_address()
     }
 }
